736f4b35f19e86f967d720254ff092942717115f
Roman <roman.developer@mail.ru>
2025-12-06 14:53:02 +0300

    test

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  con;
+  sole.log(234 + 22);
+
+  try {
+  } catch {}
+
+  const abc = 1;
+  const port = 3000;
+
+  await app.listen(port);
+}
+
+bootstrap();
+
+const a: Bet = 1;
