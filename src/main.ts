ec60080e6b793ea15d895980fdcfc59fc046fe9a
Roman <roman.developer@mail.ru>
2025-12-22 09:09:19 +0300

    chore(prisma): add prisma entities docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 import * as cookieParser from 'cookie-parser';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
 
 import { ConfigService } from '@/shared/modules/config';
 
@@ -24,11 +27,21 @@ async function bootstrap() {
   if (configService.get('hasDocs')) {
     const swaggerConfig = new DocumentBuilder()
       .addBearerAuth()
-      .setTitle('Meet Point app backend API')
+      .setTitle('Meet Point')
+      .setDescription('Backend API docs')
+      .setExternalDoc('Prisma entities docs', 'docs/prisma')
       .build();
 
     const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
     SwaggerModule.setup('docs', app, swaggerDocument);
+
+    const prismaEntitiesDocument = yaml.load(
+      readFileSync(
+        join(process.cwd(), './src/shared/modules/generated/prisma-docs/openapi.yaml'),
+        'utf8',
+      ),
+    ) as OpenAPIObject;
+    SwaggerModule.setup('docs/prisma', app, prismaEntitiesDocument);
   }
 
   await app.listen(configService.get('port'));
